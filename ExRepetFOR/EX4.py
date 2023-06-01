676129b298577fbaf0cfde59fb0f8b9337ba7afd
LucasAltino <124930759+LucasAltino@users.noreply.github.com>
2023-06-01 10:18:42 -0300

    Delete ExRepetFOR directory

--- ExRepetFOR/EX4.py
+++
@@ -1,5 +0,0 @@
-lista = []
-for n in range(0,101,7):
-    if n %5 != 0:
-        lista.append(n)
-print("Números: ", lista)
--- ExRepetFOR/ex1.py
+++
@@ -1,6 +0,0 @@
-pares = []
-
-for n in range(0,101, 2):
-    pares.append(n)
-print("Números pares:", pares)
-print("Quantidade:", n)
